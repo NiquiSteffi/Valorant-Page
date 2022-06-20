1dd3bdd770a35bab14c38aa04e71f285075a117e
Nicole Perez <niqui.steffi@hotmail.com>
2022-06-20 03:51:11 -0300

    agregue estilos, header y logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/valorant-logo.png';
+import styles from '../styles/Header.module.css';
 
 export function Header() {
   return (
-    <ul>
-      <li><Link to="/">HOME</Link></li>
-      <li><Link to= "/">nose</Link></li>
-      <li></li>
-      <li></li>
+    <ul className={styles.principalContainer}>
+      <li className={styles.liContainer}><Link to="/"><img src={logo} alt="" className={styles.logo} /></Link></li>
+      <li className={styles.liContainer}><Link className={styles.linkContainer} to= "/">MAPAS</Link></li>
+      <li className={styles.liContainer}><Link className={styles.linkContainer} to= "/">ARMAS</Link></li>
     </ul>
   )
 }
